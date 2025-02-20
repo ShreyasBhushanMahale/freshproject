72571290a0802c8d0d39afd4f30cf4ba35c86971
ShreyasBhushanMahale <shreyasmahale03@gmail.com>
2025-02-20 22:33:28 +0530

    Added a new message label to the form

--- mynewapp/views.py
+++ mynewapp/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
 from .forms import ContactForm
-from .models import Contact # import the Contact model, the name should be the same as the class name in models.py!!!
+from .models import (
+    Contact,
+)  # import the Contact model, the name should be the same as the class name in models.py!!!
 
 # Create your views here.
 
+
 def contact_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            print("name:", form.cleaned_data['name'])
-            print("age:", form.cleaned_data['age'])
-            print("email:", form.cleaned_data['email'])
-            print("message:", form.cleaned_data['message'])
-            Contact.objects.create(name=form.cleaned_data['name'], age=form.cleaned_data['age'], email=form.cleaned_data['email'], message=form.cleaned_data['message'])
-            return render(request, 'success.html')
-    
+            print("name:", form.cleaned_data["name"])
+            print("age:", form.cleaned_data["age"])
+            print("email:", form.cleaned_data["email"])
+            print("message/opinion:", form.cleaned_data["message"])
+            Contact.objects.create(
+                name=form.cleaned_data["name"],
+                age=form.cleaned_data["age"],
+                email=form.cleaned_data["email"],
+                message=form.cleaned_data["message"],
+            )
+            return render(request, "success.html")
+
     else:
         form = ContactForm()
-    return render(request, 'contact.html', {'form': form})
-    
+    return render(request, "contact.html", {"form": form})
